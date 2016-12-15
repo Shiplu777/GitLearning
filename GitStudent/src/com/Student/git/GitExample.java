b9e129f64bd08b34ce29362719a3c25e5db39af7
Shiplu <shiplu@bfs.om>
2016-12-15 16:37:27 -0500

    Added another name Mahmuda

--- GitStudent/src/com/Student/git/GitExample.java
+++ GitStudent/src/com/Student/git/GitExample.java
@@ -4,7 +4,7 @@ public class GitExample {
 
 	public static void main(String[] args) {
 		
-		String[] Names = {"Shiplu","Nusrat","Noman"};
+		String[] Names = {"Shiplu","Nusrat","Noman","Mahmuda"};
 		
 		for(int i=0;i<Names.length;i++){
 			System.out.println(Names[i]);
