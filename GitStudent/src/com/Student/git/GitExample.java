9603c2aea7a9b76d81954ceb54fb64d6d11fd220
Shiplu <shiplu@bfs.om>
2016-12-15 16:32:36 -0500

    I have added one GitExample Class

---
+++ GitStudent/src/com/Student/git/GitExample.java
@@ -0,0 +1,15 @@
+package com.Student.git;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		
+		String[] Names = {"Shiplu","Nusrat","Noman"};
+		
+		for(int i=0;i<Names.length;i++){
+			System.out.println(Names[i]);
+		}
+
+	}
+
+}
